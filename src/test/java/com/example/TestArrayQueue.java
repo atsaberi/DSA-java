9aad999e8c79226100f2edecdbd83f2f2033fb95
Atieh <atieh.saberi@gmail.com>
2025-08-12 16:59:17 -0400

    Fix test file name

--- src/test/java/com/example/TestArrayQueue.java
+++ src/test/java/com/example/TestArrayQueue.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class testArrayQueue {
+class TestArrayQueue {
 
     @Test
     void testEnqueue() {
